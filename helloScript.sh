efea4101e98cbbd13cc34dd96bf9153f91130130
naruto <kiptootitus75@gmail.com>
2025-04-09 04:44:18 +0300

    confitions

--- helloScript.sh
+++ helloScript.sh
@@ -6,9 +6,23 @@ byee titus..
 krative
 
 
-count = 10
+count=10
 
-if [ $count -eq 10 ]
+if (( $count > 9 ))
 then
 		echo "The condition is true"
-fi		
+elif(( $count => 9 ))
+then
+		echo "The condition is tru"
+else
+		echo "The condition is false"
+fi
+
+age=30
+
+if [ "$age" -lt 18 ] && [ "$age" -lt 40 ]
+then
+		echo "Age is correct"
+else
+		echo "Age is not correct"
+fi
